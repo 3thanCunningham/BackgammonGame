2274abc4e777e6e81b20ac14a2da92f1287d23d2
ethncunningham <115724625+ethncunningham@users.noreply.github.com>
2022-11-16 20:49:37 +0000

    adding display colour file

---
+++ DisplayColour.java
@@ -0,0 +1,7 @@
+package backgammon;
+
+public class DisplayColour {
+    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String WHITE = "\033[0;37m";   // WHITE
+}
