e0ac128b6324b1393eedab514f49a45fecbf00bf
jerome-manzano <jerome.manzano@ucdconnect.ie>
2022-11-22 18:52:55 +0000

    New Player Class

--- Player.java
+++ Player.java
@@ -1,13 +1,17 @@
 package backgammon;
 
 public class Player {
+	
+	private BoardType pipNumbers;
 	private String name;
 	private int roll;
-	
+	private int pipCount;
 	
 	Player (String name){
 		this.name = name;
+		pipCount = 167;
 		roll = 1;
+		pipNumbers = BoardType.CLOCKWISE;
 	}
 	
 	public String getName() {
@@ -22,4 +26,20 @@ public class Player {
 		this.roll = roll;
 	}
 	
+	public void setBoard(BoardType pipNumbers) {
+		this.pipNumbers = pipNumbers;
+	}
+	
+	public BoardType getBoardType(){
+		return pipNumbers;
+	}
+	
+	public void setPipCount(int pipCount) {
+		this.pipCount = pipCount;
+	}
+	
+	public int getPipCount() {
+		return pipCount;
+	}
 }
+	
