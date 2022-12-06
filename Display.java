54da5f5fef530dec5060eb1ca155e90540285d18
ethncunningham <115724625+ethncunningham@users.noreply.github.com>
2022-12-06 16:07:20 +0000

    refactoring display class

--- Display.java
+++ Display.java
@@ -21,9 +21,37 @@ public class Display {
 		
 		if(player.getBoardType() == BoardType.ANTICLOCKWISE) {
 		System.out.println("\n  12   11   10   9    8    7        6    5    4    3    2    1 ");
+		}
+		else {
+		System.out.println("\n  13   14   15   16   17   18      19   20   21   22   23   24 ");
+		}
 		System.out.println("  ------------------------------------------------------------");
 		int MaxSize = board.maxStackSize();
-		for(int i = 0;  i < MaxSize ; ++i) {
+		
+		for(int m = 0; m < MaxSize ; ++m) {
+			for(int n = 12; n <= 23; ++n) {
+				
+				 if(n == 17) {
+				    	if( board.StackIndexEmpty(n,m) == true)
+				    	{
+				    		System.out.print("  *   || ");
+				    	}
+				    	else
+						System.out.print(board.getStack(n).get(m).toString()+" || ");
+					}
+					
+				    else if( board.StackIndexEmpty(n,m) == true)
+						System.out.print("  *  ");
+				
+				else 
+					System.out.print(board.getStack(n).get(m).toString());
+					
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=MaxSize-1;  i>=0 ; --i) {
 			for(int j = 11; j >= 0; --j) {
 				
 				
@@ -45,86 +73,14 @@ public class Display {
 			}
 			System.out.println();
 		}
-		System.out.println();
-		
-		for(int m = MaxSize-1; m >= 0 ; --m) {
-			for(int n = 12; n <= 23; ++n) {
-				
-				 if(n == 17) {
-				    	if( board.StackIndexEmpty(n,m) == true)
-				    	{
-				    		System.out.print("  *   || ");
-				    	}
-				    	else
-						System.out.print(board.getStack(n).get(m).toString()+" || ");
-					}
-					
-				    else if( board.StackIndexEmpty(n,m) == true)
-						System.out.print("  *  ");
-				
-				else 
-					System.out.print(board.getStack(n).get(m).toString());
-					
-			}
-			System.out.println();
-		}
 		System.out.println("  ------------------------------------------------------------");
+		if(player.getBoardType() == BoardType.ANTICLOCKWISE) {
 		System.out.println("  13   14   15   16   17   18      19   20   21   22   23   24 \n");
 		}
-		
 		else {
-			System.out.println("\n  13   14   15   16   17   18      19   20   21   22   23   24 ");
-			System.out.println("  ------------------------------------------------------------");
-			int MaxSize = board.maxStackSize();
-			for(int i = 0;  i < MaxSize ; ++i) {
-				for(int j = 11; j >= 0; --j) {
-					
-					
-				    if(j == 6) {
-				    	if( board.StackIndexEmpty(j,i) == true)
-				    	{
-				    		System.out.print("  *   || ");
-				    	}
-				    	else
-						System.out.print(board.getStack(j).get(i).toString()+" || ");
-					}
-					
-				    else if( board.StackIndexEmpty(j,i) == true)
-						System.out.print("  *  ");
-					
-					else 
-						System.out.print(board.getStack(j).get(i).toString());
-						
-				}
-				System.out.println();
-			}
-			System.out.println();
-			
-			for(int m = MaxSize-1; m >= 0 ; --m) {
-				for(int n = 12; n <= 23; ++n) {
-					
-					 if(n == 17) {
-					    	if( board.StackIndexEmpty(n,m) == true)
-					    	{
-					    		System.out.print("  *   || ");
-					    	}
-					    	else
-							System.out.print(board.getStack(n).get(m).toString()+" || ");
-						}
-						
-					    else if( board.StackIndexEmpty(n,m) == true)
-							System.out.print("  *  ");
-					
-					else 
-						System.out.print(board.getStack(n).get(m).toString());
-						
-				}
-				System.out.println();
-			}
-			System.out.println("  ------------------------------------------------------------");
 			System.out.println("  12   11   10   9    8    7        6    5    4    3    2    1 \n");
-			}
 		}
+	}
 	
 	
 	public String getCommand(Player player) {
