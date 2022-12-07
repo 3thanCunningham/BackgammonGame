f9eb67b3662ba6fa74f9dfbb30e06c2797d82ab7
jerome-manzano <jerome.manzano@ucdconnect.ie>
2022-12-07 09:35:48 +0000

    Fixed bugs in main class, added hint and pip count

--- BackGammon.java
+++ BackGammon.java
@@ -28,9 +28,9 @@ public class BackGammon {
 			do {
 				for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
 
+					display.displayBoard(board, player[i]);
+					
 					do {
-						display.displayBoard(board, player[i]);
-						System.out.println(player[i].getName() + " , Enter 'r' to roll, or 'q' to quit: ");
 
 						input = display.getCommand(player[i]);
 						command.setCommand(input);
@@ -38,7 +38,18 @@ public class BackGammon {
 						if (command.isQuit()) {
 							break;
 						} else if (!command.isValid()) {
-							System.out.println("Invalid Syntax - Try Again");
+							System.out.println("\nInvalid Syntax - Try Again\n");
+							game.endTurn(false);
+							
+						} else if (command.isHint()) {
+							System.out.println("\nEnter 'r' to roll\nEnter 'p' for your pip count\nEnter 'q' to quit\n");
+							game.endTurn(false);
+							
+						} else if (command.isPipCount()) {
+							System.out.println("\n" + player[i].getName()+ "'s Pip Count : " + player[i].getPipCount(board,player[i]) + "\n");
+							game.endTurn(false);
+							
+							
 						} else if (command.isRoll()) {
 							player[i].setRoll(dice.roll(), dice.roll());
 
@@ -87,21 +98,21 @@ public class BackGammon {
 										display.displayBoard(board, player[i]);
 										} 
 									else {
-										System.out.println("Invalid input - try again");
+										System.out.println("\nInvalid input - try again\n");
 										} 
 									} 
 									while(!isMoveDone);
 									}
 								else { 
-									System.out.println("No Valid Moves - Skipping Your Turn"); 
+									System.out.println("\nNo Valid Moves - Skipping Your Turn\n"); 
 									moves=0;
 								}
 							}
 							while(moves>0);
-							game.endTurn();
+							game.endTurn(true);
 							}
 						 else {
-							System.out.println("Invalid Command");
+							System.out.println("\nInvalid Command\n");
 						}
 					} while (!game.isTurnOver());
 				}
@@ -109,13 +120,14 @@ public class BackGammon {
 
 			while (!command.isQuit() && !board.isGameOver());
 
-			System.out.println("GAME OVER!");
+			System.out.println("\nGAME OVER!\n");
 			System.out.println("PLAYER 1. You decide. Would you like to play again? (y/n): ");
 			input = display.getCommand(player[0]);
 			command.setCommand(input);
 
 		}
 		while (command.isReplay());
+		System.out.println("\nThanks for playing! Goodbye.\n");
 
 }
 }
